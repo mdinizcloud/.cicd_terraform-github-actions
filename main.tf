887310478478c9ae4c9b5a6d8b79d0056e927f3a
Marcelo <marcelo.diniz.ti@gmail.com>
2023-07-04 09:12:51 -0300

    Marcelo

--- main.tf
+++ main.tf
@@ -52,6 +52,17 @@ resource "aws_security_group" "web-sg" {
   }
 }
 
+resource "aws_instance" "ec2-vm" {
+  ami                         = data.aws_ssm_parameter.linuxAmi.value
+  instance_type               = "t3.micro"
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.sg.id]
+  subnet_id                   = aws_subnet.subnet.id
+  tags = {
+    Name = "${terraform.workspace}-ec2"
+  }
+}
+
 output "web-address" {
   value = "${aws_instance.web.public_dns}:8080"
 }
